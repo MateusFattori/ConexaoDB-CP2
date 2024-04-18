65ddfb11c7909c0bf2d528eea51eca5b68d808c1
Mateus Fattori <83764798+MateusFattori@users.noreply.github.com>
2024-04-17 22:54:04 -0300

    Add files via upload
    DTO e Controler feita - commit (Mateus)

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cp2.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace cp2.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ILogger<LoginController> _logger;
+        private readonly DataContext _dataContext;
+
+        public LoginController(ILogger<LoginController> logger, DataContext dataContext)
+        {
+            _dataContext = dataContext;
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Login(User resquest)
+        {
+            var user = _dataContext.MVC_Users.FirstOrDefault(x => x.UserEmail == resquest.UserEmail);
+            if (user != null) {
+                return BadRequest("Usuário já existe");
+            }
+
+            return View();
+        }
+
     }
 }
---
+++ DTOs/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cp2.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string USerEmail { get; set; }
+        [DataType(DataType.Password)]
+        public string UserPassaword { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("UserPassaword", ErrorMessage = "As senhas não coincidem.")]
+        public string ComparePassaword { get; set; }
+        public string UserPhone { get; set; }
+    }
+}
